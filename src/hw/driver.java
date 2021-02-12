97e50b147946e2ea1f1b373944d875549af15588
Eevee-py <71530384+Eevee-py@users.noreply.github.com>
2021-02-12 07:25:33 -0600

    Initial commit

---
+++ src/hw/driver.java
@@ -0,0 +1,28 @@
+package hw;
+
+
+public class Hw1 {
+
+	public static void main(String[] args) {
+		String s = "Hey, you. You're finally awake. You were trying to cross the border, right? "
+	    			  + "Walked right into that Imperial ambush, same as us, and that thief over there. "
+				      + "Oh Yea! And also the gospel of Luke. I don't want to delete the lines above. HA!";
+		int x = 0;
+	    
+	    for (int i = 0; i < s.length(); ++i) {
+	    	char y = s.charAt(i);
+
+	      
+	    	if (y == 'a' || 
+	    		y == 'e' || 
+	    		y == 'i' || 
+	    		y == 'o' || 
+	    		y == 'u') 
+	           {++x;}
+
+	     
+	      }
+	    System.out.println("There are " + x + " vowels in the first lines of Skyrim plus the last line "
+	    		          +"that has the New Testament book.");  
+	  }
+}
